788e3e1a0bb51fa4a10dd13e6839fc8f54a51105
Undiy <undiy@yandex.ru>
2023-08-01 17:44:22 +0500

    Classes

--- HelloKotlin/src/main/kotlin/spices.kt
+++ HelloKotlin/src/main/kotlin/spices.kt
@@ -1,17 +1,7 @@
-class SimpleSpice {
-    var name: String = "curry"
-    var spiciness: String = "mild"
-    val heat: Int
-        get() = if (spiciness == "mild") {
-            5
-        } else {
-            0
-        }
-}
-
-class Spice(
-    var name: String = "curry",
-    var spiciness: String = "mild") {
+sealed class Spice(
+    var name: String,
+    var spiciness: String
+) {
     val heat: Int
         get() = when (spiciness)  {
             "hot" -> 10
@@ -23,14 +13,50 @@ class Spice(
     init {
         println("Name: $name, spiciness: $spiciness, heat: $heat")
     }
+
+    abstract fun prepareSpice()
 }
 
-fun makeSalt() = Spice("salt", "bland")
+class Curry(color: SpiceColor = YellowSpiceColor) : Spice("curry", "mild"),
+    Grinder,
+    SpiceColor by color {
+    override fun prepareSpice() {
+        super.grind()
+    }
+}
+class Salt : Spice("salt", "bland"), Grinder {
+    override fun prepareSpice() {
+        super.grind()
+    }
+}
+
+interface Grinder {
+    fun grind() {
+        println("grinding... done")
+    }
+}
+
+interface SpiceColor {
+    val color: Color
+}
+
+object YellowSpiceColor : SpiceColor {
+    override val color = Color.YELLOW
+}
+
+class SpiceContainer(private val spice: Spice) {
+    val label: String
+        get() = spice.name
+}
+
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF),
+    YELLOW(0xFFFF00);
+}
 
 fun main() {
-    val spice = SimpleSpice()
-
-    println("Spiciness: ${spice.spiciness}, heat: ${spice.heat}")
-
-    val spices = listOf(Spice(), makeSalt(), Spice("cayene", "hot"))
+    val spices = listOf(Curry(), Salt())
+    val containers = spices.map { SpiceContainer(it) }.forEach {
+        println(it.label)
+    }
 }
